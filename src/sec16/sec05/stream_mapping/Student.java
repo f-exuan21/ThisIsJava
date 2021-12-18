1e14a813129522e562026ce4497d88fb3f100dae
f-exuan21 <f.exuan21@gmail.com>
2021-12-18 23:20:59 +0900

    chapter 16. 스트림과 병렬처리-1

---
+++ src/sec16/sec05/stream_mapping/Student.java
@@ -0,0 +1,19 @@
+package sec16.sec05.stream_mapping;
+
+public class Student {
+    private String name;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
